5add63a92d9feae89af44f1d6dc275dcca4d7777
Nathan Evans <nathan01944@me.com>
2021-09-14 19:41:27 -0400

    deleted extra files

--- pages/api/Odds/index.js
+++
@@ -1,9 +0,0 @@
-import { formatted } from "../../../data/formatted"
-
-export default function handler(req, res) {
-    if (formatted) {
-        res.status(200).json(formatted)
-      } else {
-        res.status(404).json({ message: `Data not found.` })
-      }
-  }  
